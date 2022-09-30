6d796c95eb412bdcf14a48af6a37ec6cbc5c9cab
irenabartunkova <irena.bartunkova@gmail.com>
2022-09-30 14:29:09 +0200

    Náhodný obrázek a citát - 1.

--- src/main/java/cz/czechitas/java2webapps/ukol2/Kolekce.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/Kolekce.java
@@ -3,7 +3,7 @@ package cz.czechitas.java2webapps.ukol2;
 import java.util.List;
 
 public class Kolekce {
-    private static final List<String> quotes = List.of(
+    private static final List<String> nahodnyCitat = List.of(
             "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
             "A user interface is like a joke. If you have to explain it, it's not that good.",
             "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
