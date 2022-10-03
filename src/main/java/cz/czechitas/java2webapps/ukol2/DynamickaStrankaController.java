0b18ac0fd9c26b02a4a57ba38406c6b0b54080ca
irenabartunkova <irena.bartunkova@gmail.com>
2022-10-03 14:43:46 +0200

    Náhodný obrázek a citát.

--- src/main/java/cz/czechitas/java2webapps/ukol2/DynamickaStrankaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/DynamickaStrankaController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.swing.*;
+import java.util.List;
 import java.util.Random;
 
 
@@ -15,11 +16,21 @@ public class DynamickaStrankaController {
     @GetMapping("/")
 
     private ModelAndView nahodnyObrazek() {
-        int nahodnyObrazek = random.nextInt(4) + 1;
+
+        int nahodnyObrazekCitat = random.nextInt(4) + 1;
+
+        List<String> citaty = List.of(
+                "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
+                "A user interface is like a joke. If you have to explain it, it's not that good.",
+                "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
+                "I have a joke on programming but it only works on my computer."
+        );
 
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("obrazek", nahodnyObrazek);
+        modelAndView.addObject("obrazek", nahodnyObrazekCitat);
+        modelAndView.addObject("citaty", citaty.get(nahodnyObrazekCitat));
         return modelAndView;
 
-    }
+}
+
 }
